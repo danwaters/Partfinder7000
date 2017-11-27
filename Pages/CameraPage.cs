97cc5f2c552837e992061387d3158b7c158fbecc
Dan Waters <dawate@microsoft.com>
2017-11-27 15:24:10 -0600

    Initial commit from PetID

---
+++ Pages/CameraPage.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Partfinder7000.Pages
+{
+    public partial class CameraPage : ContentPage
+    {
+        // See CameraPage.cs in iOS project for native camera implementation
+        public CameraPage()
+        {
+
+        }
+    }
+}
---
+++ Partfinder7000Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Partfinder7000
+{
+    public partial class Partfinder7000Page : ContentPage
+    {
+        public Partfinder7000Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
